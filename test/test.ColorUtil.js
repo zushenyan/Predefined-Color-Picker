beb240c7745431406a8173ebbab205111e3fbc91
Andrew Yan <zushenyan@gmail.com>
2015-10-22 11:31:26 +0800

    make travis CI happy

--- test/test.ColorUtil.js
+++ test/test.ColorUtil.js
@@ -3,15 +3,26 @@ var pcp = require("../dist/js/Main");
 var ColorUtil = pcp.ColorUtil;
 
 describe("ColorUtil", function(){
-	var goodColors = ["#ffffff", "", "#aaaaaa", "#bbbbbb"];
-	var badColors = ["#ffffff", "#aaa", "#bbbbbb"];
+	var goodColors = [
+		{color: "#ffffff"	, name: "meow"},
+		{color: ""				, name: "test"},
+		{color: "#aaaaaa" , name: ""},
+		{color: "#bbbbbb"	, name: "qqqq"}
+	];
+	var badColors = [
+		{color: "#ffffff"	, name: ""},
+		{color: "#aaa"		, name: "test"},
+		{color: "#bbbbbb"	, name: "wow"}
+	];
 
 	describe("isHexColors", function(){
 		it("should work", function(){
-			var result = ColorUtil.isHexColors(goodColors);
+			var colors = goodColors.map(function(obj){ return obj.color; });
+			var result = ColorUtil.isHexColors(colors);
 			expect(result).to.be.true;
 		});
 		it("should not work", function(){
+			var colors = badColors.map(function(obj){ return obj.color; });
 			var result = ColorUtil.isHexColors(badColors);
 			expect(result).to.be.false;
 		});
